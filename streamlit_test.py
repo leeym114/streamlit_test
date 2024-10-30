e9f6cebeba8a3268ddc24ac9265c801bcce7445c
leeym114 <84678701+leeym114@users.noreply.github.com>
2024-10-30 19:21:16 +0900

    Add files via upload

---
+++ streamlit_test.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("에버영코리아 4대 법정 의무교육")
+st.subheader("개인정보보호 유튜브 영상 재생")
+st.text("중장년층 개인정보보호 교육")
+
+video_url = "https://www.youtube.com/watch?v=JFcj-KQAHeA"
+st.video(video_url)
+
